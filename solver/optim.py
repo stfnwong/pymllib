Added adam update and unit test. Added dropout test. Softmax loss has bug so loss goes to inf during testing

solver/optim.py
@@ -57,8 +57,31 @@ def rmsprop(x, dx, config=None):
     return next_x, config
 
 def adam(x, dx, config=None):
-    pass
 
+    if config is None:
+        config = {}
+
+    config.setdefault('learning_rate', 1e-2)
+    config.setdefault('m', np.zeros_like(x))
+    config.setdefault('v', np.zeros_like(x))
+    config.setdefault('epsilon', 1e-8)
+    config.setdefault('beta1', 0.9)
+    config.setdefault('beta2', 0.999)
+    config.setdefault('t', 0)
+
+    lr = config['learning_rate']
+    b1 = config['beta1']
+    b2 = config['beta2']
+    eps = config['epsilon']
+
+    config['t'] += 1
+    config['m'] = b1 * config['m'] + (1 - b1) * dx
+    config['v'] = b2 * config['v'] + (1 - b2) * (dx**2)
+    mt_hat = config['m'] / (1 - (b1**config['t']))
+    vt_hat = config['v'] / (1 - (b2**config['t']))
+    next_x = x - lr * mt_hat / (np.sqrt(vt_hat + eps))
+
+    return next_x, config
 
 
 

tests/test_solver.py
@@ -189,6 +189,44 @@ class TestSolver(unittest.TestCase):
 
     def test_adam(self):
         print("\n======== TestSolver.test_adam:")
+
+        N = 4
+        D = 5
+        w = np.linspace(-0.4, 0.6, num=N*D).reshape(N, D)
+        dw = np.linspace(-0.6, 0.4, num=N*D).reshape(N, D)
+        m = np.linspace(0.6, 0.9, num=N*D).reshape(N, D)
+        v = np.linspace(0.7, 0.5, num=N*D).reshape(N, D)
+        config = {'learning_rate': 1e-2, 'm': m, 'v': v, 't': 5}
+
+        next_w, _ = optim.adam(w, dw, config=config)
+        expected_next_w = np.asarray([
+        [-0.40094747, -0.34836187, -0.29577703, -0.24319299, -0.19060977],
+        [-0.1380274,  -0.08544591, -0.03286534,  0.01971428,  0.0722929 ],
+        [ 0.1248705,   0.17744702,  0.23002243,  0.28259667,  0.33516969],
+        [ 0.38774145,  0.44031188,  0.49288093,  0.54544852,  0.59801459]])
+        expected_v = np.asarray([
+        [ 0.69966,     0.68908382,  0.67851319,  0.66794809,  0.65738853,],
+        [ 0.64683452,  0.63628604,  0.6257431,   0.61520571,  0.60467385,],
+        [ 0.59414753,  0.58362676,  0.57311152,  0.56260183,  0.55209767,],
+        [ 0.54159906,  0.53110598,  0.52061845,  0.51013645,  0.49966    ]])
+        expected_m = np.asarray([
+        [ 0.48,        0.49947368,  0.51894737,  0.53842105,  0.55789474],
+        [ 0.57736842,  0.59684211,  0.61631579,  0.63578947,  0.65526316],
+        [ 0.67473684,  0.69421053,  0.71368421,  0.73315789,  0.75263158],
+        [ 0.77210526,  0.79157895,  0.81105263,  0.83052632,  0.85      ]])
+
+        next_w_error = error.rel_error(next_w, expected_next_w)
+        v_error = error.rel_error(config['v'], expected_v)
+        m_error = error.rel_error(config['m'], expected_m)
+
+        print("next_w_error = %f" % next_w_error)
+        print("v_error = %f" % v_error)
+        print("m_error = %f" % m_error)
+
+        self.assertLessEqual(next_w_error, self.eps)
+        self.assertLessEqual(v_error, self.eps)
+        self.assertLessEqual(m_error, self.eps)
+
         print("======== TestSolver.test_adam: <END> ")
 
     def test_all_optim(self):
@@ -196,7 +234,7 @@ class TestSolver(unittest.TestCase):
 
         dataset =  load_data(self.data_dir, self.verbose)
 
-        optim_list = ['sgd', 'sgd_momentum', 'rmsprop']
+        optim_list = ['sgd', 'sgd_momentum', 'adam', 'rmsprop']
         num_train = 50
 
         small_data = {
@@ -211,7 +249,7 @@ class TestSolver(unittest.TestCase):
         hidden_dims = [100, 50, 10]     # just some random dims
         weight_scale = 5e-2
         learning_rate = 1e-2
-        num_epochs = 50
+        num_epochs = 30
         batch_size = 50
         solvers = {}
 