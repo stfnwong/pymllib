04a57e413d3c89639880a890d78916b7f1d7b944
Stefan Wong <stfnwong@gmail.com>
2017-11-16 19:06:24 +1300

    Implemented a routine to take a variety of solver outputs and make a single large plot of them.

--- examples/ex_vis_checkpoints.py
+++ examples/ex_vis_checkpoints.py
@@ -3,32 +3,51 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 #import numpy as np
+import matplotlib.pyplot as plt
 
-import pymllib.solver.solver as solver
-import pymllib.vis.vis_solver as solver_utils
-#import pymllib.utils.solver_utils as solver_utils
+from pymllib.solver import solver
+from pymllib.vis import vis_solver
 
 
-if __name__ == "__main__":
+def plot_single(cname):
 
-    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
 
-    cpath = "/home/kreshnik/Documents/compucon/machine-learning/models/2017-11-13-3ln-01"
+    title = "Layer 1 weights"
+    solv = solver.Solver(None, None)
+    solv.load_checkpoint(cname)
+    vis_solver.plot_model_first_layer(ax, solv.model, cname)
+    ax.set_title(title)
+
+    # Also plot the training results
+    fig2 = plt.figure()
+    ax2 = [ ]
+    for i in range(3):
+        subax = fig2.add_subplot(3, 1, (i+1))
+        ax2.append(subax)
+    vis_solver.plot_solver(ax2, solv)
+    plt.show()
+
+
+
+def plot_sequence():        # TODO ; add params
+
     #cpath = "/home/kreshnik/Documents/compucon/machine-learning/models/conv3fc2-2017-11-12-01"
     #cpath = "/home/kreshnik/Documents/compucon/machine-learning/models/conv4fc2-2017-11-11-02"
 
     # Get a figure and axis
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    num_checkpoints = 20        # max = 102
+    num_checkpoints = 100        # max = 102
 
     for n in range(num_checkpoints):
         title = "Layer 1 Weights (epoch %d)" % (n+1)
         #cname = cpath + '/' + "conv3fc2-check_epoch_%d.pkl" % (n+1)
-        cname = cpath + '/' + "3ln_epoch_%d.pkl" % (n+1)
+        cname = cpath + '/' + "c16-fc256-fc10-net_epoch_%d.pkl" % (n+1)
         solv = solver.Solver(None, None)
         solv.load_checkpoint(cname)
-        solver_utils.plot_model_first_layer(ax, solv.model, cname)
+        vis_solver.plot_model_first_layer(ax, solv.model, cname)
         ax.set_title(title)
         plt.pause(1)
         plt.draw()
@@ -39,5 +58,35 @@ if __name__ == "__main__":
     for i in range(3):
         subax = fig2.add_subplot(3, 1, (i+1))
         ax2.append(subax)
-    solver_utils.plot_solver(ax2, solv)
+    vis_solver.plot_solver(ax2, solv)
     plt.show()
+
+
+def load_solver(fname):
+    solv = solver.Solver(None, None)
+    solv.load_checkpoint(fname)
+
+    return solv
+
+
+if __name__ == "__main__":
+    prefix = "/home/kreshnik/Documents/compucon/machine-learning/models"
+    cpath = ["conv-net-train-2017-11-15-01", "conv-net-train-2017-11-15-02"]
+    cname = ['c16-fc256-fc10-net', 'c16-c32-fc256-fc10-net', 'c16-c32-c64-fc256-fc256-fc10-net', 'c16-c32-c64-c128-fc256-fc256-fc10-net']
+
+
+    solver_dict = {}
+    for p in cpath:
+        for n in cname:
+            cpoint_filename = prefix + '/' + p + '/' + n + '_epoch_100.pkl'
+            solv = load_solver(cpoint_filename)
+            solver_dict[n + p[-1]] = solv
+
+    # Plot solvers against each other
+    fig, ax = vis_solver.get_train_fig()
+    vis_solver.plot_solver_compare(ax, solver_dict)
+    fig.tight_layout()
+    plt.show()
+
+    #plot_sequence()
+    #plot_single(cpoint_filename)
