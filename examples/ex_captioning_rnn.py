bf74bf5ad3b73257dd36d602377d80d496517879
ML_KNET <stfnwong@gmail.com>
2017-12-17 14:54:20 +1300

    Added methods to sample at test time

--- examples/ex_captioning_rnn.py
+++ examples/ex_captioning_rnn.py
@@ -15,13 +15,35 @@ import matplotlib.pyplot as plt
 from pymllib.classifiers import captioning_rnn
 from pymllib.solver import captioning_solver
 from pymllib.utils import coco_utils
+from pymllib.utils import image_utils
 
 # Debug
 #from pudb import set_trace; set_trace()
 
+def test_time_sampling(data, model, batch_size=16, plot_figures=False):
+
+    for split in ['train', 'val']:
+        minibatch = coco_utils.sample_coco_minibatch(data,
+                        split=split,
+                        batch_size=batch_size)
+        gt_captions, features, urls = minibatch
+        # Ground-truth captions
+        gt_captions = coco_utils.decode_captions(gt_captions, data['idx_to_word'])
+        # Sample captions
+        sample_captions = model.sample(features)
+        sample_captions = sample_captions.astype(np.int32)
+        sample_captions = coco_utils.decode_captions(sample_captions, data['idx_to_word'])
+
+        if plot_figures:
+            for gt, samp, url in zip(gt_captions, sample_captions, urls):
+                plt.imshow(image_utils.image_from_url(url))
+                plt.title('Split : %s\nSample : %s\nGT: %s' % (split, samp, gt))
+                plt.axis('off')
+                plt.show()
+
 def ex_caption_rnn(verbose=False, plot_figures=False):
 
-    test_data = coco_utils.load_coco_data(max_train=5000)
+    test_data = coco_utils.load_coco_data(max_train=50000)
 
     small_rnn_model = captioning_rnn.CaptioningRNN(
         cell_type='rnn',
@@ -44,6 +66,9 @@ def ex_caption_rnn(verbose=False, plot_figures=False):
     )
 
     solv.train()
+    test_time_sampling(test_data,
+                       small_rnn_model,
+                       plot_figures=plot_figures)
     # Plot the loss...
     if plot_figures:
         plt.plot(solv.loss_history, 'o')
@@ -51,6 +76,7 @@ def ex_caption_rnn(verbose=False, plot_figures=False):
         plt.ylabel('Loss')
         plt.title('Captioning RNN Loss')
         plt.show()
+
 
 if __name__ == '__main__':
     global_verbose = True
--- pymllib/utils/rnn_utils.py
+++ pymllib/utils/rnn_utils.py
@@ -6,8 +6,11 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pymllib.layers import rnn_layers
+from pymllib.utils import coco_utils
+from pymllib.utils import image_utils
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def check_loss(N, T, V, p, verbose=False):
     x = 0.001 * np.random.randn(N, T, V)
@@ -19,3 +22,32 @@ def check_loss(N, T, V, p, verbose=False):
         print(out)
 
     return out
+
+
+def test_time_sampling(data, model, batch_size=16):
+
+    captions = {}
+    for split in ['train', 'val']:
+        minibatch = coco_utils.sample_coco_minibatch(data,
+                        split=split,
+                        batch_size=batch_size)
+        gt_captions, features, urls = minibatch
+        # Ground-truth captions
+        gt_captions = coco_utils.decode_captions(gt_captions, data['idx_to_word'])
+        # Sample captions
+        sample_captions = model.sample(features)
+        sample_captions = sample_captions.astype(np.int32)
+        sample_captions = coco_utils.decode_captions(sample_captions, data['idx_to_word'])
+
+        captions['%s-sample' % str(split)] = sample_captions
+        captions['%s-gt' % str(split)] = gt_captions
+
+    return captions
+
+def plot_test_time_samples(gt_captions, sample_captions, urls, split):
+
+    for gt, samp, url in zip(gt_captions, sample_captions, urls):
+        plt.imshow(image_utils.image_from_url(url))
+        plt.title('Split : %s\nSample : %s\nGT: %s' % (split, samp, gt))
+        plt.axis('off')
+        plt.show()
